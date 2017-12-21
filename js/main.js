d40f3fc2ceb139e899c2e1366cb0c509502a677e
Alissa Powers <aapowers@uci.edu>
2017-12-21 07:46:29 -0800

    switches to use sections instead of pages and updates content

--- js/main.js
+++ js/main.js
@@ -31,3 +31,12 @@ window.onclick = function(event) {
         }
     }
 })();
+
+//Scroll to section
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+});
